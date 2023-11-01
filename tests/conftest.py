a15b4357de2e7808619abbd6579c0e99dae2beb9
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-11-01 23:15:21 +0300

    Added REMOTE_BROWSER_URL

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,9 +42,10 @@ def setup_browser(request):
 
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
+    remote_browser_url = os.getenv('REMOTE_BROWSER_URL')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@{remote_browser_url}",
         options=options
     )
 
